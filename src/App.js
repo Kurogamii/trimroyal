4a6d3de2e0108404427be3c3c6a1e543e03339df
Kurogamii <joylove.rightchoice@gmail.com>
2023-01-03 18:40:38 +0100

    site

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ function App() {
       <div className="logo">TRIM</div>
       <nav className="navigation">
         <ul>
-          <li className={currentPage === '' ? 'active' : null}><a href="/">Home</a></li>
-          <li className={currentPage === '/about' ? 'active' : ''}><a href="/about">About</a></li>
-          <li className={currentPage === '/contact' ? 'active' : ''}><a href="/contact">Contact</a></li>
+          <li ><a href="/">Home</a></li>
+          <li ><a href="/about">Services</a></li>
+          <li ><a href="/about">About</a></li>
+          <li ><a href="/contact">Contact</a></li>
         </ul>
       </nav>
     </header>
